60e94b0bba82a390a24abef75dc59aa328f9ed23
zaquari_andl <zaquariandl043@gmail.com>
2024-02-29 14:11:34 -0600

    added models appointment and barber and tested to see if able to seed

--- server/app.py
+++ server/app.py
@@ -1,15 +1,23 @@
-#!/usr/bin/env python3
+from flask import Flask, request, make_response
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+from models import db, Barber, Appointment
+import os
 
-# Standard library imports
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+DATABASE = os.environ.get(
+    "DB_URI", f"sqlite:///{os.path.join(BASE_DIR, 'app.db')}")
 
-# Remote library imports
-from flask import request
-from flask_restful import Resource
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
 
-# Local imports
-from config import app, db, api
-# Add your model imports
+migrate = Migrate(app, db)
 
+db.init_app(app)
+
+api = Api(app)
 
 # Views go here!
 
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,32 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, Barber, Appointment
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
-        print("Starting seed...")
+        print("Seeding barber...")
+        barbers = [
+            Barber(name="Tyler",
+                   image="https://upload.wikimedia.org/wikipedia/commons/3/35/Basic_human_drawing.png",
+                   phone="123-456-7890",
+                   email="notanemail@gmail.com")
+        ]
+        
+        db.session.add_all(barbers)
+
+        print("Seeding appointments...")
+        appointments = [
+            Appointment(time = '2:15',
+                        hc_notes = 'Make me look like a pineapple.',
+                        barber_id = 1)
+        ]
+
+        db.session.add_all(appointments)
+        db.session.commit()
+
+        print('Done seeding!..')
         # Seed code goes here!
+
+

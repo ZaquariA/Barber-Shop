548ed507587dacd933c3a28646e73e7f53f8b4d9
+ <TylerCharlton1993@gmail.com>
2024-03-03 17:16:19 -0500

    Added Customer validations

--- server/models.py
+++ server/models.py
@@ -58,19 +58,43 @@ class Appointment(db.Model, SerializerMixin):
             raise ValueError("Invalid time format. Please use the format 'H:MM' where H is between 1-12 and MM is between 00-59.")
         return time
     
-    class Customer(db.Model, SerializerMixin):
-        __tablename__ = 'customers'
+class Customer(db.Model, SerializerMixin):
+    __tablename__ = 'customers'
 
-        id = db.Column(db.Integer, primary_key = True)
-        name = db.Column(db.String, nullable = False)
-        preferred_haircut = db.Column(db.String)
-        phone_number = db.Column(db.String)
-        email = db.Column(db.String)
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String, nullable = False)
+    preferred_haircut = db.Column(db.String)
+    phone_number = db.Column(db.String)
+    email = db.Column(db.String)
 
-        appointments = db.relationship('Appointment', back_populates = 'customer')
+    appointments = db.relationship('Appointment', back_populates = 'customer')
 
-        serialize_rules = ('-appointments.customer', )
+    serialize_rules = ('-appointments.customer', )
+    
+    @validates('name', 'preferred_haircut', 'phone_number', 'email')
+    def validate_name(self, key, name):
+        if not name:
+            raise ValueError('Name cannot be empty.')
+        return name
+    
+    def validate_haircut(self, key, preferred_haircut):
+        if not preferred_haircut:
+            raise ValueError('Please enter a preferred haircut.')
+        return preferred_haircut
+
+    def validate_phone_number(self, key, phone_number):
+        if not re.match(r'^\d{3}-\d{3}-\d{4}$', phone_number):
+            raise ValueError("Please enter a 10-digit phone number with hyphens (e.g., 123-456-7890).")
+        return phone_number
+    
+    def validate_email(self, key, email):
+        if not re.match(r'^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$', email):
+            raise ValueError("Please enter a valid email address")
+        return email
+    
+    
+
+
 
         
-
       

bacffa1ba73d498650f38328b51e96fdc3436497
+ <TylerCharlton1993@gmail.com>
2024-03-06 18:21:55 -0500

    Made Code Cuts re route you back to main page when clicked.

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,12 +1,15 @@
+// Header.js
 import React from 'react';
-import styles from './react.css'
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
-        <div className="header_div">    
-            <h1 className="title">Code Cuts</h1>
-        </div>
+    <div className="header_div">
+      <Link to="/">
+        <h1 className="title">Code Cuts</h1>
+      </Link>
+    </div>
   );
-};
+}
 
 export default Header;

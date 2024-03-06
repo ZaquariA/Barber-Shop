eba575e00f355dcf86f992ef2b617f0dc0968383
+ <TylerCharlton1993@gmail.com>
2024-03-06 12:44:05 -0500

    Bug fixes on barber name breaking the app if none assigned.

--- client/src/components/Appointment.js
+++ client/src/components/Appointment.js
@@ -17,8 +17,8 @@ function Appointment() {
                     <div className="appointment_div" key={appointment.id}>
                         <h1 className="appointment_time">{appointment.time}</h1>
                         <h1 className="appointment_hc_notes">{appointment.hc_notes}</h1>
-                        <h1 className="appointment_barber_id">{appointment.barber_id}</h1>
-                        <h1 className="appointment_haircut_id">{appointment.haircut_id}</h1>
+                        <h1 className="appointment_barber_name">{appointment.barber ? appointment.barber.name : 'No Barber Assigned'}</h1>
+                        <h1 className="appointment_haircut_id">{appointment.haircut.name}</h1>
                     </div>
                 )
             })}
